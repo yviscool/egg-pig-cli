73c3b196545521e2b642b564d75a2a743577ea39
Y.V <5196666qwe@gmail.com>
2019-06-26 17:32:24 +0800

    chore: remove controller default

--- files/controller/__name__.ts
+++ files/controller/__name__.ts
@@ -2,7 +2,7 @@ import { IService, EggAppConfig, Application, Context } from 'egg';
 import { Controller, Get } from 'egg-pig';
 
 @Controller()
-export default class <%= name %>Controller {
+export class <%= name %>Controller {
 
     constructor(
         private ctx: Context,
